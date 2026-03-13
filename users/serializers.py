a1b461c660e68d8441de5c49cf324c0d40fab6cb
TizianaNapoli <napolitizy@gmail.com>
2026-03-13 16:11:25 +0100

    feat: add user registration API endpoint with serializer and view.

---
+++ users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'role')
+        extra_kwargs = {
+            'role': {'read_only': True}  # Il ruolo viene impostato a CLIENT di default nel modello
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
