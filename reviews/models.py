666b809beefc151b5691d267e0238d4c95e802f7
Linda <lindagiannonea7@gmail.com>
2026-03-13 13:16:45 +0100

    add review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Review(models.Model):
+    class ReviewStatus(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        APPROVED = 'APPROVED', 'Approved'
+        HIDDEN = 'HIDDEN', 'Hidden'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title=models.CharField(max_length=20,blank=True)
+    vote = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+
+    description=models.TextField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+
+    status = models.CharField(
+        max_length=10,
+        choices=ReviewStatus.choices,
+        default=ReviewStatus.PENDING
+    )
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    product = models.ForeignKey(
+        "catalog.Product",
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "product"],
+                name="unique_user_product_review"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} - {self.product} ({self.vote})"
+    
