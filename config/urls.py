6c66fca221633a110899d1468923b804567c7504
Sabbycor <sabrinacorrado5@gmail.com>
2026-03-16 17:35:16 +0100

    feat(catalog): aggiorna requirements, settings, urls e api-contract

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from catalog.views import CategoryViewSet, ProductViewSet
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    #TokenObtainPairView, TokenRefreshView = path per accedere come superuser usando il token
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
+
