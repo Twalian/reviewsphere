9e0affd812a283359649153d8e33e24502a33a42
TizianaNapoli <napolitizy@gmail.com>
2026-03-17 13:19:20 +0100

    fix: Fixed a ValueError caused by multiple DefaultRouter instances by switching to SimpleRouter in the users app.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from .views import RegisterView, MeView, UserAdminViewSet
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'admin', UserAdminViewSet, basename='user-admin')
 
 urlpatterns = [
